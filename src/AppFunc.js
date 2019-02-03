c9b3a8fdae0beacbfd6a6d06329d203a751c2f1a
ryanboris <ryan.boris@gmail.com>
2019-02-03 05:22:05 -0500

    updates components

--- src/AppFunc.js
+++ src/AppFunc.js
@@ -1,18 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from 'reactstrap';
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+};
+
 const AppFunc = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
-  console.group('useEffect()');
+  const [status, setStatus] = useState(navigator.onLine);
+  const [{ latitude, longitude, speed }, setLocation] = useState(initialLocationState);
+  let mounted = true;
+
   useEffect(() => {
     document.title = `You have clicked ${count} times.`;
     window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+    navigator.geolocation.getCurrentPosition(handleGeolocation);
+    navigator.geolocation.watchPosition(handleGeolocation);
+    const watchId = navigator.geolocation.watchPosition(handleGeolocation);
+
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+      navigator.geolocation.clearWatch(watchId);
+      mounted = false;
     };
   }, [count]);
+
+  const handleGeolocation = event => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  };
+
+  const handleOnline = () => {
+    setStatus(true);
+  };
+
+  const handleOffline = () => {
+    setStatus(false);
+  };
 
   const handleMouseMove = event => {
     setMousePosition({
@@ -47,6 +84,16 @@ const AppFunc = () => {
       />
 
       <div>{JSON.stringify(mousePosition, null, 2)}</div>
+
+      <h2>Network status</h2>
+      <p>
+        You are <strong>{status ? 'online' : 'offline'}</strong>
+      </p>
+
+      <h2>Geolocation</h2>
+      <p>Latitude is: {latitude}</p>
+      <p>Longitude is {longitude}</p>
+      <p>Your speed is: {speed ? speed : '0'}</p>
     </>
   );
 };
