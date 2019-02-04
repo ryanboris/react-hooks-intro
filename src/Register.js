219b6878b6015d0349528d8742881c08424f77c2
ryanboris <ryan.boris@gmail.com>
2019-02-04 02:07:55 -0500

    🎨 reworks register page with hooks

--- src/Register.js
+++ src/Register.js
@@ -4,32 +4,55 @@ import { Button } from "reactstrap";
 export default function Register() {
   const [form, setForm] = useState({
     username: "",
-    email: "",
-    password: ""
+    password: "",
+    email: ""
   });
+
+  const [user, setUser] = useState(null);
+
+  const handleChange = event => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setUser(form);
+    setForm({ username: "", password: "", email: "" });
+  };
+
   return (
     <>
       <h2>Register</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Username"
-          onChange={e => e.target.value}
+          name="username"
+          onChange={handleChange}
+          value={form.username}
         />
         <input
           type="password"
           placeholder="Password"
-          onChange={e => e.target.value}
+          name="password"
+          onChange={handleChange}
+          value={form.password}
         />
         <input
           type="email"
           placeholder="Email"
-          onChange={e => e.target.value}
+          name="email"
+          onChange={handleChange}
+          value={form.email}
         />
         <Button outline color="primary" size="sm" type="submit">
           Submit
         </Button>
       </form>
+      {user && JSON.stringify(user, null, 4)}
     </>
   );
 }
